cba8765610904429f516d8bb291f40fe83260555
Cam <cameronmcouch@gmail.com>
2021-05-12 07:15:21 -0400

    placed event handler logic into functions drag, drop. Need to continue to work on box-model calculations for left and top offsets within drag

--- scripts/shelf.js
+++ scripts/shelf.js
@@ -1,58 +1,62 @@
 "use strict";
 //when the json has been returned as response, store response in a variable within shelf.js
 var waitForResponse = setInterval(
-  function() {
-    if(jsonResponse) {
-      clearInterval(waitForResponse);
-      if(window.sessionStorage.getItem('hideSvcShelf') == true || document.cookie.indexOf('hideSvcShelf') > -1) {
-          return;
-      }
-      buildShelf(jsonResponse);
-    } else { console.log('Waiting for response...'); }
-  }, 10);
+    function () {
+        if (jsonResponse) {
+            clearInterval(waitForResponse);
+            if (window.sessionStorage.getItem('hideSvcShelf') == true || document.cookie.indexOf('hideSvcShelf') > -1) {
+                return;
+            }
+            buildShelf(jsonResponse);
+        } else { console.log('Waiting for response...'); }
+    }, 10);
 
-  function buildShelf(customizations) {
+function buildShelf(customizations) {
 
     var customShelf = {
-      animate: customizations.animate,
-      leftArrowColor: customizations.leftArrowColor,
-      rightArrowColor: customizations.rightArrowColor,
-      courseBorder: customizations.courseBorder,
-      courseColor: customizations.courseColor,
-      fontFamily: customizations.fontFamily,
-      fontColor: customizations.fontColor,
-      hasScrollTop: customizations.hasScrollTop,
-      moveable: customizations.moveable, 
-      shelfState: {
-        lifted: false
-      },
-      segmentBorder: customizations.segmentBorder,
-      segments: customizations.segments,
-      segmentColor: customizations.segmentColor,
-      sessionStorage: customizations.sessionStorage,
-      startingPosition: customizations.startingPosition,
-      type: customizations.type,
-      xColor: customizations.xColor,
-      width: 0,
-      height: 0,
+        animate: customizations.animate,
+        leftArrowColor: customizations.leftArrowColor,
+        rightArrowColor: customizations.rightArrowColor,
+        courseBorder: customizations.courseBorder,
+        courseColor: customizations.courseColor,
+        fontFamily: customizations.fontFamily,
+        fontColor: customizations.fontColor,
+        hasScrollTop: customizations.hasScrollTop,
+        moveable: customizations.moveable,
+        shelfState: {
+            lifted: false,
+            expanded: false
+        },
+        segmentBorder: customizations.segmentBorder,
+        segments: customizations.segments,
+        segmentColor: customizations.segmentColor,
+        sessionStorage: customizations.sessionStorage,
+        startingPosition: customizations.startingPosition,
+        type: customizations.type,
+        xColor: customizations.xColor,
+        width: {
+            collapsed: 0,
+            expanded: 0
+        },
+        height: 0,
     },
-// Setting up markup
-    // googleFont = customShelf['fontFamily']['google'] ? customShelf['fontFamily']['name'] : undefined;
-    // if(googleFont) {
-    //     document.head.append('<link href="https://fonts.googleapis.com/css2?family=' + googleFont + '&display=swap" rel="stylesheet">');
-    // }
-    shelf = document.createElement('div'),
-    shelfMarkup = customShelf.shelfMarkup ? customShelf.shelfMarkup : "<table id='menu-wrapper'><tbody><tr id='main-menu'></tr></tbody></table>",
-    segmentsContainers = [],
-    segmentsButtons = [],
-    scrollTopArrow = customShelf.hasScrollTop ? document.createElement('button') : undefined,
-    closeX = document.createElement('button'),
-    expand = document.createElement('button');
+        // Setting up markup
+        // googleFont = customShelf['fontFamily']['google'] ? customShelf['fontFamily']['name'] : undefined;
+        // if(googleFont) {
+        //     document.head.append('<link href="https://fonts.googleapis.com/css2?family=' + googleFont + '&display=swap" rel="stylesheet">');
+        // }
+        shelf = document.createElement('div'),
+        shelfMarkup = customShelf.shelfMarkup ? customShelf.shelfMarkup : "<table id='menu-wrapper'><tbody><tr id='main-menu'></tr></tbody></table>",
+        segmentsContainers = [],
+        segmentsButtons = [],
+        scrollTopArrow = customShelf.hasScrollTop ? document.createElement('button') : undefined,
+        closeX = document.createElement('button'),
+        expand = document.createElement('button');
 
-    customShelf.segments.forEach(function(segment) { var cell = document.createElement('td'); segmentsContainers.push(cell); });
-    customShelf.segments.forEach(function(segment) { var button = document.createElement('button'); segmentsButtons.push(button); });
+    customShelf.segments.forEach(function (segment) { var cell = document.createElement('td'); segmentsContainers.push(cell); });
+    customShelf.segments.forEach(function (segment) { var button = document.createElement('button'); segmentsButtons.push(button); });
 
-// Attribute setup
+    // Attribute setup
     shelf.setAttribute('id', 'shelf');
     shelf.setAttribute('class', customShelf.startingPosition);
     shelf.style.background = customShelf.courseColor;
@@ -69,49 +73,91 @@ var waitForResponse = setInterval(
     closeX.style.color = customShelf.xColor;
     closeX.innerText = '✕';
     // Button type for all buttons segments
-    segmentsButtons.forEach(function(segment) { segment.setAttribute('type', 'button'); customShelf.segmentColor ? segment.style.backgroundColor = customShelf.segmentColor : '' });
+    segmentsButtons.forEach(function (segment) { segment.setAttribute('type', 'button'); customShelf.segmentColor ? segment.style.backgroundColor = customShelf.segmentColor : '' });
     // Build shelf
-    segmentsContainers.forEach(function(segment, idx) { segment.appendChild(segmentsButtons[idx]); });
+    segmentsContainers.forEach(function (segment, idx) { segment.appendChild(segmentsButtons[idx]); });
     shelf.appendChild(expand);
     shelf.insertAdjacentHTML("beforeend", shelfMarkup);
     shelf.appendChild(scrollTopArrow);
     shelf.appendChild(closeX);
-    
+
     var shelfTable = shelf.childNodes[1],
         tableBody = shelfTable.childNodes[0],
         tableRow = tableBody.childNodes[0];
-    
+
     // apply customizations to each drawer
-    segmentsContainers.forEach(function(segment, idx) { 
-        segment.firstChild.innerText = Object.keys(customShelf.segments[idx]); 
+    segmentsContainers.forEach(function (segment, idx) {
+        segment.firstChild.innerText = Object.keys(customShelf.segments[idx]);
         segment.firstChild.style.color = customShelf.fontColor;
         segment.firstChild.style.fontFamily = customShelf.fontFamily.name;
-        segment.setAttribute('class','segment-container');
+        segment.setAttribute('class', 'segment-container');
         segment.firstChild.setAttribute('class', 'segment');
-        tableRow.appendChild(segment); 
+        tableRow.appendChild(segment);
     });
-    
+
+    /**
+     * @param {*} shelfObj A reference to the custom shelf object passed in from the mouseup event handler 
+     */
+
+    function drop(shelfObj) {
+        shelfObj.shelfState.lifted = false;
+        this.style.cursor = 'grab';
+    }
+
+    /**
+     * @param {*} shelfObj A reference to the custom shelf object passed in from the mousemove event handler
+     * @param {*} evt mouse event
+     */
+
+    function drag(shelfObj, evt) {
+        if (shelfObj.shelfState.lifted) {
+            if (evt.screenX <= window.innerWidth) {
+                if (shelfObj.shelfState.expanded) {
+                    if (evt.screenX >= 0 + shelfObj.width.expanded * .75) {
+                        this.style.left = evt.screenX - shelfObj.width.expanded * .75 + 'px';
+                    } else { drop.call(this, shelfObj); this.style.left = "0px" }
+                } else {
+                    if (evt.screenX >= 0 + shelfObj.width.collapsed * .55) {
+                        this.style.left = evt.screenX - shelfObj.width.collapsed * .55 + 'px';
+                    }
+                }
+            } else { 
+                drop.call(this, shelfObj); 
+                this.style.left = shelfObj.shelfState.expanded ? evt.screenX - shelfObj.width.expanded * .95 + 'px' : evt.screenX - shelfObj.width.collapsed * 1.25 + 'px';
+            }
+            if (evt.screenY <= window.innerHeight) {
+                if(evt.screenY >= 0 + shelfObj.height) {
+                    this.style.top = evt.screenY - shelfObj.height * 1.5 + 'px';
+                }
+            } else {
+                console.log(evt.screenY, window.innerHeight);
+                drop.call(this, shelfObj); 
+                this.style.top = 0 +'px';
+            }
+        }
+    }
+
     expand.addEventListener('click', function (evt) {
-        evt.stopPropagation();
+        this.shelfState.expanded = !this.shelfState.expanded;
         shelfTable.style.display = shelfTable.style.display === 'inline-block' ? 'none' : 'inline-block';
-    }, false);
+    }.bind(customShelf), false);
 
     expand.addEventListener('touchstart', function (evt) {
+        this.shelfState.expanded = !this.shelfState.expanded;
         shelfTable.style.display = shelfTable.style.display === 'inline-block' ? 'none' : 'inline-block';
-
-    }, false);
+    }.bind(customShelf), false);
 
     window.addEventListener('scroll', function (evt) {
         var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-        if(scrollTop > 0) {
+        if (scrollTop > 0) {
             scrollTopArrow.classList.add('right-arrow-scrolled');
         }
-        if(scrollTop == 0) {
+        if (scrollTop == 0) {
             scrollTopArrow.classList.value.includes('right-arrow-scrolled') ? scrollTopArrow.classList.remove('right-arrow-scrolled') : '';
         }
     }, false);
-    
-    scrollTopArrow.addEventListener('click', function(evt) {
+
+    scrollTopArrow.addEventListener('click', function (evt) {
         window.scrollTo({
             top: 0,
             left: 0,
@@ -119,7 +165,7 @@ var waitForResponse = setInterval(
         });
     }, false);
 
-    scrollTopArrow.addEventListener('touchstart', function(evt) {
+    scrollTopArrow.addEventListener('touchstart', function (evt) {
         window.scrollTo({
             top: 0,
             left: 0,
@@ -128,10 +174,10 @@ var waitForResponse = setInterval(
         evt.stopPropagation();
     }, false);
 
-    closeX.addEventListener('click', function(evt) {
+    closeX.addEventListener('click', function (evt) {
         customShelf.shelfState.hidden = true;
         shelf.classList.add('hidden-by-user');
-        if(customShelf.sessionStorage) {
+        if (customShelf.sessionStorage) {
             try {
                 window.sessionStorage.setItem('hideSvcShelf', true);
             } catch (e) { console.warn(e); }
@@ -151,35 +197,16 @@ var waitForResponse = setInterval(
         this.style.cursor = 'grabbing';
     }, false);
 
-    shelf.addEventListener('mouseup', function(evt) {
-        customShelf.shelfState.lifted = false;
-        this.style.cursor = 'grab';
-    }, false);
+    shelf.addEventListener('mouseup', drop.bind(shelf, customShelf), false);
 
-    shelf.addEventListener('touchend', function(evt) {
-        customShelf.shelfState.lifted = false;
-        this.style.cursor = 'grab';
-    }, false);
+    shelf.addEventListener('touchend', drop.bind(shelf, customShelf), false);
 
-    window.addEventListener('mousemove', function(e){
-    if(customShelf.shelfState.lifted) {
-        if(e.screenX < window.innerWidth - customShelf.width * .62 && e.screenX > 0 + customShelf.width * .55) {
-            this.style.left = e.screenX - 50 +'px';
-        }
-        if(e.screenY < window.innerHeight && e.screenY > 0 + customShelf.height * 2) {
-            this.style.top = e.screenY - customShelf.height * 2 + 'px';
-        }
-    }
-    }.bind(shelf), false);
+    window.addEventListener('mousemove', drag.bind(shelf, customShelf), false);
 
-    window.addEventListener('touchmove', function(e){
-    if(customShelf.shelfState.lifted) {
-        this.style.left = e.touches[0].clientX - 50 +'px';
-        this.style.top = e.touches[0].clientY - 25 +'px';
-    }
-    }.bind(shelf), false);
+    window.addEventListener('touchmove', drag.bind(shelf, customShelf), false);
 
     document.body.insertBefore(shelf, document.body.firstChild);
-    customShelf.width = shelf.offsetWidth;
+    customShelf.width.collapsed = shelf.offsetWidth;
+    customShelf.width.expanded = shelf.offsetWidth + customShelf.segments.length * 50;
     customShelf.height = shelf.offsetHeight;
-  }
+}
